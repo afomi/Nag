fe36d34736929a2e78fcf163bb2d153722ca7dab
Ryan Wold <wold@afomi.com>
2011-12-04 12:52:37 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nag::Application.config.secret_token = '08f7c73d06bc831d6671b85ccd0c61b260f8768ad61e4f506ab809ddddb1c779ed7f7c2c3aca673e7ab21b7099f76717eb1c460eef73eed8b28811a3d20986c8'
