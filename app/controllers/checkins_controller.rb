79c522678d8f889386e02035ffb9a0a13cd31a30
Ryan Wold <wold@afomi.com>
2012-03-03 15:55:50 -0800

    add a /data method to CheckinsController to provide the 100 latest checkins in a Simile format

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,83 +1,97 @@
 class CheckinsController < ApplicationController
-  # GET /checkins
-  # GET /checkins.xml
+
   def index
     @checkins = Checkin.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @checkins }
+      format.xml { render :xml => @checkins }
     end
   end
 
-  # GET /checkins/1
-  # GET /checkins/1.xml
   def show
     @checkin = Checkin.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @checkin }
+      format.xml { render :xml => @checkin }
     end
   end
 
-  # GET /checkins/new
-  # GET /checkins/new.xml
   def new
     @checkin = Checkin.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @checkin }
+      format.xml { render :xml => @checkin }
     end
   end
 
-  # GET /checkins/1/edit
   def edit
     @checkin = Checkin.find(params[:id])
   end
 
-  # POST /checkins
-  # POST /checkins.xml
   def create
     @checkin = Checkin.new(params[:checkin])
 
     respond_to do |format|
       if @checkin.save
         format.html { redirect_to(new_checkin_path, :notice => 'Checkin was successfully created.') }
-        format.xml  { render :xml => @checkin, :status => :created, :location => @checkin }
+        format.xml { render :xml => @checkin, :status => :created, :location => @checkin }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @checkin.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @checkin.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /checkins/1
-  # PUT /checkins/1.xml
   def update
     @checkin = Checkin.find(params[:id])
 
     respond_to do |format|
       if @checkin.update_attributes(params[:checkin])
         format.html { redirect_to(@checkin, :notice => 'Checkin was successfully updated.') }
-        format.xml  { head :ok }
+        format.xml { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @checkin.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @checkin.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /checkins/1
-  # DELETE /checkins/1.xml
   def destroy
     @checkin = Checkin.find(params[:id])
     @checkin.destroy
 
     respond_to do |format|
       format.html { redirect_to(checkins_url) }
-      format.xml  { head :ok }
+      format.xml { head :ok }
     end
+  end
+
+  # this controller method is designed to provide Nag data in a format
+  # readable by Simile timeline library
+  def data
+    @checkins                  = Checkin.hundred_latest
+    @simile_formatted_checkins = []
+
+    @checkins.each do |checkin|
+      @simile_formatted_checkins << {
+          :title         => DateTime.parse(checkin["created_at"].to_s).strftime("%b %d at %I:%M %P"),
+          :start         => checkin["created_at"],
+          :description   => checkin["text"],
+          :image         => "",
+          :link          => "",
+          :durationEvent => false
+      }
+    end
+
+    payload = {
+        'dateTimeFormat' => 'iso8601',
+        'wikiURL'        => "http://afomi.com",
+        'wikiSection'    => "Ryan's Life' Timeline",
+        "events"         => @simile_formatted_checkins
+    }
+    render :json => payload
   end
 end
