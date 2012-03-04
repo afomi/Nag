e839833a1949d6a519d48f759a710e695048c02c
Ryan Wold <wold@afomi.com>
2012-03-03 16:48:31 -0800

    move user closer to a custom setting,

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :basic_settings
+
+  def basic_settings
+    @settings = {
+        :user     => "Ryan",
+        :timeline => {
+            :wiki_section => "Ryan's Checkin Timeline'"
+        }
+    }
+  end
 end
