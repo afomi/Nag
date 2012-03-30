4dd19408d3e59c52d0acb95cd162381b34fa9fb4
Ryan Wold <wold@afomi.com>
2012-03-30 11:35:53 -0700

    add Help page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Nag::Application.routes.draw do
 
-  resources :touchpoints
-
+  match "help", :to => "status#help"
   get "checkins/data"
 
   match "status/:id", :controller => "status", :action => "edit", :id => "id"
 
   resources :checkins
+  resources :touchpoints
 
   root :to => "checkins#new"
 end
