fce1b074f34ee19f20bcf8925d0e06b4a2e0cfa6
Ryan Wold <wold@afomi.com>
2012-04-07 14:14:38 -0700

    add trello hover box to show all board counts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+
+// Button Behaviors in the UI
+
+$(function () {
+     console.log('something');
+    $("#trello_total").hover(
+        function () {
+            console.log('over');
+            $("#trello_hover").css("display", "block");
+        },
+        function () {
+            console.log('out');
+            $("#trello_hover").css("display", "none");
+        }
+    );
+
+});
+
