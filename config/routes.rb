f5b0dc12530313dc2f1853729aba2dbfd945fce3
Ryan Wold <wold@afomi.com>
2015-12-29 13:53:36 -0800

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,18 @@ Nag::Application.routes.draw do
   resources :documents
   resources :habits
 
-  get "tags", :to => "checkins#tags"
-  match "help", :to => "status#help"
-  get "just_login", :to => "checkins#just_login"
-  get "logout", :to => "checkins#logout"
+  get "help", to: "status#help"
+
+  get "tags", to: "checkins#tags"
+  get "just_login", to: "checkins#just_login"
+  get "logout", to: "checkins#logout"
   get "checkins/data"
 
-  match "status/:id", :controller => "status", :action => "edit", :id => "id"
-  match "status/:app/:key/:value/", :controller => "status", :action => "update", :app => "app", :key => "id", :value => "value"
+  get "status/:id", :controller => "status", :action => "edit", :id => "id"
+  get "status/:app/:key/:value/", :controller => "status", :action => "update", :app => "app", :key => "id", :value => "value"
 
   resources :checkins
   resources :touchpoints
 
-  root :to => "checkins#new"
+  root to: "checkins#new"
 end
