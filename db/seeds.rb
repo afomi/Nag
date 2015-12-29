7efd61f4b74821e1ce8fac846f20ef20ba4f5149
Ryan Wold <wold@afomi.com>
2015-12-29 14:19:04 -0800

    at 1 Stat to start
    and update ruby hash syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@
 # seeds should have enough data to make a working system that demonstrates EVERY feasible functional state of the app
 
 checkin = {
-  :text       => "Here's a check-in" + rand(1000).to_s,
-  :project_id => 0
+  text: "Here's a check-in" + rand(1000).to_s,
+  project_id: 0
 }
 
 touchpoint = {
-  :key         => ["trello", "gmail"].sample.to_s,
-  :value       => "complete",
-  :description => "description" + rand(1000).to_s
+  key: ["trello", "gmail"].sample.to_s,
+  value: "complete",
+  description: "description" + rand(1000).to_s
 }
 
 20.times do
@@ -18,6 +18,11 @@ end
 
 checkins = Checkin.all
 20.times do
-  c = checkins.sample
-  Touchpoint.create(touchpoint.merge!(:checkin_id => c.id))
+  new_checkin = checkins.sample
+  Touchpoint.create(touchpoint.merge!(checkin_id: new_checkin.id))
 end
+
+Stat.create!({
+  key: "task",
+  app: "google"
+})
